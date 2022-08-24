3964ef19ef5e5bd506a053f195a5386f9ba7432d
shijianhang <3076679995@qq.com>
2022-08-24 14:46:40 +0800

    升级版本

--- SeleniumBoot/__init__.py
+++ SeleniumBoot/__init__.py
@@ -4,7 +4,7 @@ from .validator import Validator
 from .boot import Boot
 
 __author__ = "shigebeyond"
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 __description__ = "SeleniumBoot: make an easy way (yaml) to web automation testing"
 
 __all__ = [
